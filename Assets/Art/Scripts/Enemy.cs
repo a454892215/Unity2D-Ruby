ec2f17cdc7053aab6d134476d2c12934e90c0428
L <unkown@.com>
2019-09-11 10:16:36 +0800

    初始化Ruby

---
+++ Assets/Art/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float changeDirTime = 2;
+    public float speed = 500f;
+    public bool isHorizontal = true;
+    private Vector3 originV3;
+    Rigidbody2D m_rigidbody2D;
+    void Start()
+    {
+        originV3 = transform.position;
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private float movedTime = 500f;
+    private
+    void Update()
+    {
+        if (movedTime < changeDirTime)
+        {
+            movedTime += Time.deltaTime;
+            Vector2 position = m_rigidbody2D.position;
+            if (isHorizontal)
+            {
+                position.x = position.x + Time.deltaTime * speed;
+
+            }
+            else
+            {
+                position.y = position.y + Time.deltaTime * speed;
+            }
+            m_rigidbody2D.MovePosition(position);
+        }
+        else
+        {
+            movedTime = 0;
+            speed = -speed;
+        }
+
+
+    }
+}
---
+++ Assets/Art/Scripts/PlayerContr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContr : MonoBehaviour
+{
+    Rigidbody2D m_rigidbody2D;
+    public float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+         m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = m_rigidbody2D.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        m_rigidbody2D.MovePosition(position);
+    }
+}
