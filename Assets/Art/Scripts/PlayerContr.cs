83d9c88ed673377ac5a42be323eb9c88071484f7
L <unkown@.com>
2019-09-13 18:35:02 +0800

    --

--- Assets/Art/Scripts/PlayerContr.cs
+++ Assets/Art/Scripts/PlayerContr.cs
@@ -7,6 +7,7 @@ public class PlayerContr : MonoBehaviour
     Rigidbody2D m_rigidbody2D;
     public float speed = 3.0f;
     Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +21,21 @@ public class PlayerContr : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = m_rigidbody2D.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
-
-        animator.SetFloat("moveX", horizontal);
-        animator.SetFloat("moveY", vertical);
 
         m_rigidbody2D.MovePosition(position);
 
